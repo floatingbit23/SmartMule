70404a6e3f1546bdbf2070f0c6ea0ba503f42530
floatingbit23 <javisancab2003@gmail.com>
2026-04-30 20:11:39 +0200

    test: Delete hashing stress test

--- smartmule_launcher.sh
+++ smartmule_launcher.sh
@@ -18,11 +18,11 @@ if [ -f "smartmule.pid" ]; then
     
 fi
 
-# Lanzamos en segundo plano
+# Lanzamos en segundo plano utilizando el python del entorno virtual
 # - nohup: Evita que el proceso muera al cerrar la sesión
 # - & : Lo manda al background
 # - > /dev/null 2>&1 : Silencia la salida (ya que SmartMule tiene sus propios logs)
-nohup python3 main.py start > /dev/null 2>&1 &
+nohup ./venv/bin/python3 main.py start > /dev/null 2>&1 &
 
 echo "[i] SmartMule lanzado en segundo plano."
 echo "[i] Puedes ver el estado en 'smartmule.log' o usar 'python3 main.py stop' para detenerlo."
